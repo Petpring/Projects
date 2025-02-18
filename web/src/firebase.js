297b74dbca717bb2604368148fc155adefc16c08
Petpring <petpring.m@kkumail.com>
2025-02-18 10:59:32 +0700

    Add files via upload

---
+++ web/src/firebase.js
@@ -0,0 +1,24 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+// import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBcU6xE0qaj_Cm2CQrPsMvyDSEg8zsWX_c",
+    authDomain: "sc362202project2024.firebaseapp.com",
+    projectId: "sc362202project2024",
+    storageBucket: "sc362202project2024.firebasestorage.app",
+    messagingSenderId: "1073660061425",
+    appId: "1:1073660061425:web:de231b427a0c34a6d16b50"
+  };
+
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+
+// export { auth, db, GoogleAuthProvider };
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
